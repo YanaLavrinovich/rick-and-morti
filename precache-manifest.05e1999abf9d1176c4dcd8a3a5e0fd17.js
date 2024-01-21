84a1a395ec579c8fa2f561438718a6f28685f600
Yana Laurynovich <ylaurynovich@godaddy.com>
2024-01-22 01:29:35 +0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.05e1999abf9d1176c4dcd8a3a5e0fd17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fa156ee9a7341c4657883101cad6dc",
+    "url": "/rick-and-morti/index.html"
+  },
+  {
+    "revision": "46c2823c8b20dd1056d2",
+    "url": "/rick-and-morti/static/css/main.78911b23.chunk.css"
+  },
+  {
+    "revision": "9fd32ed6f7c9a16bf329",
+    "url": "/rick-and-morti/static/js/2.81f7db62.chunk.js"
+  },
+  {
+    "revision": "c8c38381e12ec9a63b77",
+    "url": "/rick-and-morti/static/js/3.89dfd605.chunk.js"
+  },
+  {
+    "revision": "46c2823c8b20dd1056d2",
+    "url": "/rick-and-morti/static/js/main.4361c36f.chunk.js"
+  },
+  {
+    "revision": "1c6ffe29b88d19ad4d02",
+    "url": "/rick-and-morti/static/js/runtime~main.a7d67781.js"
+  }
+]);
